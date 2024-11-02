4d3d972f7400b507de7bcbcac341b34eb0058eec
shamsbinsohrab <aniknishan@gmail.com>
2024-11-02 22:34:44 +0100

    Fix date format

--- src/main/java/org/muslim/models/Request.java
+++ src/main/java/org/muslim/models/Request.java
@@ -1,10 +1,17 @@
 package org.muslim.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDate;
 
-public record Request(String city, String country, String method, String adjustment, LocalDate date) {
+public record Request(
+        String city,
+        String country,
+        String method,
+        String adjustment,
+        @JsonFormat(pattern = "DD/MM/YYYY") LocalDate date) {
 
     public Request(String city, String country, LocalDate date) {
-        this(city, country, null, null, date );
+        this(city, country, null, null, date);
     }
 }
